22abf74804d6d07bf8cab3a100d763cc4abf1ec0
avazbekmurataliyev <97029745+avazbekmurataliyev@users.noreply.github.com>
2022-05-26 09:17:40 +0500

    Delete fayllar directory

--- fayllar/repository/AttachmentContentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.fayllar.repository;
-
-import com.example.fayllar.model.AttachmentContent;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AttachmentContentRepository extends JpaRepository<AttachmentContent , Integer> {
-
-}
--- fayllar/repository/AttachmentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.fayllar.repository;
-
-import com.example.fayllar.model.Attachment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AttachmentRepository  extends JpaRepository<Attachment,Integer> {
-
-}
